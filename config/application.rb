f21e9c07121c162d801c3fdf1078178bbb2651dd
Tudor <tudor.roman@outlook.com>
2015-02-09 09:54:48 +0200

    Making production deploying more friendly :blush:

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,11 @@ module GArena
     config.generators do |g|
       g.template_engine :haml
     end
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+	ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
